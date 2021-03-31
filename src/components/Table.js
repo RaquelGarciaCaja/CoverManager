5fd7d50437ada1f3a8eefa3e7b0c456d9ab7ae15
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-03-31 17:39:48 +0200

    clean code

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import "../stylesheets/Floors.scss";
-import logo from "../image/chairs.png";
-
-const height = 50;
-const width = 50;
 
 class Table extends React.Component {
   constructor(props) {
@@ -13,21 +9,6 @@ class Table extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
-
-  // const getNumbers = (size) => {
-
-  //   return Math.sqrt(props.x * props.x + (props.y + props.y));
-  // }
-
-  // let target = {
-  //   x: getNumbers(width),
-  //   y: getNumbers(height),
-  // };
-  // let getDistance = (e, target) => {
-  //   let diffX = e.offsetX - target.x;
-  //   let diffY = e.offsetY - target.y;
-  //   return Math.sqrt(diffX * diffX + (diffY + diffY));
-  // };
 
   handleClick() {
     this.setState({
