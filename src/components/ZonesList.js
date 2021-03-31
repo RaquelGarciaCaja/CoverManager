48dc2b1331fd7389cb01279337ac252cf9187b28
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-03-31 09:01:29 +0200

    add new zonesList

--- src/components/ZonesList.js
+++ src/components/ZonesList.js
@@ -3,24 +3,27 @@ import "../stylesheets/Floors.scss";
 import Table from "./Table";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-
 const ZonesList = (props) => {
   // console.log(props.data);
   const tables = Object.values(props.data).map((a) => (
-     <TransformWrapper
-                  defaultScale={1}
-                  defaultPositionX={1}
-                  defaultPositionY={1}
-                >
-                  {({ zoomIn, zoomOut, ...rest }) => (
     <li className="zonesList__list" key={a.id_table}>
       {" "}
       <Table name={a.name_table} x={a.x} y={a.y} />
     </li>
-    
   ));
-  
-  return <section className="zonesList">{tables}</section>;
+
+  return (
+    <section>
+      <h5>Intenta a hacer zoom en las mesas</h5>
+      <TransformWrapper defaultScale={1} defaultPositionX={1} defaultPositionY={1}>
+        <TransformComponent>
+          <section className="zonesList">
+            <div>{tables}</div>{" "}
+          </section>
+        </TransformComponent>
+      </TransformWrapper>
+    </section>
+  );
 };
 
 export default ZonesList;
